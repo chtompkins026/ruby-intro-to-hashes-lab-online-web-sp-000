6d849c1b567a33b4f0d16ff47ece6c9d9ef0eeb0
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-19 21:06:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,9 @@ def monopoly_with_third_tier
  		monopoly = {
   		  railroads: {
   		    pieces: 4, 
-  		    names: {}, 
+  		    names: {
+  		      
+  		    }, #end of the names hash  
   		    rent_in_dollars: {
   		      one_piece_owned: 25,
             two_pieces_owned: 50,
