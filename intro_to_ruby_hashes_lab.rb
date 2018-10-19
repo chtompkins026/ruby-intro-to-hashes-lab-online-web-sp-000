e2f4a4023e307ac88f559c717c1fc41a3464e89f
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-19 21:10:46 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
   		      reading_railroad: {},
   		      pennsylvania_railroad: {},
   		      b_and_o_railroad: {},
-  		      
+  		      shortline: {} 
   		    }, #end of the names hash  
   		    
   		    rent_in_dollars: {
