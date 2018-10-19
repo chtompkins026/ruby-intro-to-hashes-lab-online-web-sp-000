594a8978eeb41c6d930176e589a0a5cd87aaac17
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-19 21:12:17 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,9 +60,11 @@ def monopoly_with_fourth_tier
   		        "mortgage_value"=>"$200"
   		      },
   		      b_and_o_railroad: {
-  		        
+  		        "mortgage_value"=>"$400"
   		      },
-  		      shortline: {} 
+  		      shortline: {
+  		        "mortgage_value"=>"$800"
+  		      } 
   		    }, #end of the names hash  
   		    
   		    rent_in_dollars: {
